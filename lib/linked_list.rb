23c484f21bd67d706200a600e03835d3ad30613f
philiptang42 <philiptang42@gmail.com>
2015-11-04 23:56:48 -0500

    some tests passing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,8 +2,31 @@ require 'pry'
 require_relative 'node'
 
 class LinkedList
-  names = LinkedList.new
-  names.prepend("Adam")
-  names.prepend("Christina")
-  names.prepend("Vikram")
+  def initialize
+    @head = nil
+  end
+
+  def each
+  node = @head
+
+    while !node.nil?
+      yield(node)
+      node = node.next_node
+    end
+  end
+
+  def prepend(value)
+    @head = Node.new(value, @head)
+  end
+
+  def [](index)
+    @result = @head
+    index.times do
+      @result = @result.next_node
+      if @result.nil?
+        return nil
+      end
+    end
+    @result
+  end
 end
